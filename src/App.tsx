9fa021a8c0181d043042c72fea9f9b42b8ce523a
Ayşe Öten <ayseguler.92@gmail.com>
2022-06-22 16:37:39 +0300

    prime react eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Chartjs from './Chartjs';
+import Navbar from './Navbar';
+import SidebarFilter from './SidebarFilter';
+import { Button } from 'primereact/button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Chartjs/> */}
+      {/* <iframe
+        src="http://192.168.20.203:8088?standalone=true"
+      >
+      </iframe> */}
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="container">
+        <div className="sidebar">
+          <SidebarFilter />
+        </div>
+        <div className="charts">
+          <Chartjs />
+          <Button>abc</Button>
+        </div>
+      </div>
     </div>
   );
 }
